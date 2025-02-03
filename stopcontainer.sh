1fbeeff73c755a512110c2e868b455b462f86bc7
Vijaygit2442 <vijayaragavan.b24@gmail.com>
2025-02-04 04:27:11 +0530

    Update stopcontainer.sh

--- stopcontainer.sh
+++ stopcontainer.sh
@@ -6,6 +6,7 @@ CONTAINER_NAME="react-ecommerce-container"
 if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
     echo "Stopping container: $CONTAINER_NAME..."
     docker stop $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
     echo "Container $CONTAINER_NAME stopped."
 else
     echo "Container $CONTAINER_NAME is not running."
